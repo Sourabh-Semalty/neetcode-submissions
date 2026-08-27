571428f375e426d2fe771379a335f12ad69ea288
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2026-08-27 22:51:47 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.ts
@@ -0,0 +1,33 @@
+class MinStack {
+    stack: number[];
+    minStack: number[];
+
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val: number): void {
+        this.stack.push(val);
+
+        const min =
+            this.minStack.length === 0
+                ? val
+                : Math.min(val, this.minStack[this.minStack.length - 1]);
+
+        this.minStack.push(min);
+    }
+
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
