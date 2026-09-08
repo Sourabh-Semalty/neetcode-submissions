6657f4400e7b5a0845a6b5bba4f9e149f0e9b525
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2026-09-08 22:45:13 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix: number[][], target: number): boolean {
+        if (!matrix || matrix.length === 0 || matrix[0].length === 0) return false;
+        for (let i = 0; i < matrix.length; i++) {
+            let low = 0;
+            let high = matrix[i].length - 1;
+
+            while (low <= high) {
+                let mid = low + Math.floor((high - low) / 2);
+                if (matrix[i][mid] === target) return true;
+                else if (matrix[i][mid] < target) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
